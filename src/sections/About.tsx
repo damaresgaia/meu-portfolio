f09fe8f754a9165e3094ca90ce6e6527d2d68ac6
Damares Gaia <damaresgaia@gmail.com>
2024-12-16 19:32:16 -0300

    feat: adiciona seção Sobre Mim

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,14 +1,64 @@
 import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+import StarIcon from "@/assets/icons/star.svg";
+import Image from "next/image";
+import JavascriptIcon from "@/assets/icons/square-js.svg";
+import HTMLIcon from "@/assets/icons/html5.svg";
+import CssIcon from "@/assets/icons/css3.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+
+const toolboxItems = [
+  {
+    title: "JavaScript",
+    icon: <JavascriptIcon />,
+  },
+  {
+    title: "HTML5",
+    icon: <HTMLIcon />,
+  },
+  {
+    title: "CSS3",
+    icon: <CssIcon />,
+  },
+  {
+    title: "React",
+    icon: <ReactIcon />,
+  },
+  {
+    title: "Github",
+    icon: <GithubIcon />,
+  },
+]
 
 
 export const AboutSection = () => {
   return (
-    <div>
+    <div className="pb-96">
       <SectionHeader
         eyebrow="Sobre mim"
         title="Desenvolvedora Full Stack"
         description="Learn more about who I am, what I do, and what inspires me."
       />
+      <div>
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>My Toolbox</h3>
+            <p>
+              Explore the technologies and tools I use to craft exceptional digital experiences.
+            </p>
+          </div>
+          <div>
+            {toolboxItems.map((item) => (
+              <div key={item.title}>
+                <span>{item.icon}</span>
+                <span>{item.title}</span>
+              </div>
+            ))}
+          </div>
+        </Card>
+      </div>
     </div>
   )
 };
