10ce492df654efc44fe4c7484de6b00fb1ac790d
Damares Gaia <damaresgaia@gmail.com>
2024-12-18 08:58:44 -0300

    feat(projects-section) adiciona animação na ProjectsSection

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -58,11 +58,14 @@ export const ProjectsSection = () => {
           description="Veja como transformei conceitos em experiências digitais envolventes."
         />
         <div className="mt-10 md:mt-20 flex flex-col gap-20">
-          {portfolioProjects.map((project) => (
+          {portfolioProjects.map((project, projectIndex) => (
             <Card
               key={project.title}
               className="px-8 pt-8 pb-0 md:pt-12 md:px-10 
-              lg:pt-16 lg:px-20"
+              lg:pt-16 lg:px-20 sticky"
+              style={{
+                top: `calc(64px + ${projectIndex * 40}px)`,
+              }}
             >
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
